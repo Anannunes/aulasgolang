89698d76528d0bec41719cf94fcf7c233573c248
Aluno Dev 25 <ana.j.goncalves12@aluno.senai.sp>
2025-03-28 08:47:20 -0300

    desafio operações

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import "fmt"   
 
 func main(){ 
-	a, b := 10. 3
+	var a int
+	var b int
+	fmt.Println("Digite um numero:")
+	fmt.Scan(&a)
+	fmt.Println("digite outro numero:")
+	fmt.Scan(&b)
 	fmt.Println(" A soma é: ", a + b)
 	fmt.Println("A subtração é:", a - b)
 	fmt.Println("a divisão é:", a / b)
-	fmt.Println("A multiplicação é:"a * b)
-	fmt.Println("O resto da divisão: "a % b)
+	fmt.Println("A multiplicação é:", a * b)
+	fmt.Println("O resto da divisão: ", a % b)
+
+}
