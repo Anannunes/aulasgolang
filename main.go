0858ce5132e5437a3dbbd3cae785f97cae1c07ed
Aluno Dev 25 <ana.j.goncalves12@aluno.senai.sp>
2025-04-10 10:26:51 -0300

    menor ou maior que 18

--- main.go
+++ main.go
@@ -3,31 +3,14 @@ package main
  import "fmt"
  
  func main() {
-    age := 45
- fmt.Println(age <=50)
- fmt.Println(age >=50)
- fmt.Println(age ==50)
- fmt.Println(age !=50)
-
- if age <50{
-    fmt.Println("menor que 30 anos")
- } else if age <40{
-    fmt.Println("menor que 40 anos") 
- }else{
-    fmt.Println("não é menos que 40 anos")
- }
-names:=[]string{"Isadora","Yasmim","Trunks","Cebolinha", "Martin","Yuri"}
-
-for index, value :=range names{
-    if index == 1{
-        fmt.Println("continua após a posição", index,"e value",value)
-        continue
-    }
-if index >2{
-    fmt.Println("sair após", index)
-    break 
+    var idade int
+	fmt.Println("digite dua idade")
+	fmt.Scan(&idade)
+	if idade <18 {
+	fmt.Println("vc é de menor")
+	}else if idade >=18 && idade <=60{
+		fmt.Println("vc é de maior")
+	}else {
+		fmt.Println("vc é um idoso")
+	}
 }
-fmt.Println("valor: ", value)
- }
-
- }
