0e7ddbed724c0464a4fe6d749f892b55d84987d1
Aluno Dev 25 <ana.j.goncalves12@aluno.senai.sp>
2025-04-11 13:31:31 -0300

    função

--- main.go
+++ main.go
@@ -1,38 +1,17 @@
 package main
  
- import "fmt"
+ import (
+	"fmt") 
  
- func main() {
-     var saldo int
-     var escolha int
-	 var numero int
-
-	 fmt.Print("digite seu saldo: ")
-	 fmt.Scan(&saldo)
-
-	 fmt.Println("Opções disponiveis:")
-	 fmt.Println("1 - sacar")
-	 fmt.Println("2 - depositar")
-	 fmt.Println("escolha uma das opções: ")
-	 fmt.Scan(&escolha)
-
-	 if escolha == 1{
-		fmt.Println("escolha um valor para sacar")
-		fmt.Scan(&numero)
-	 }
-		if  saldo > numero{
-			fmt.Println("saldo insuficiente ")
-		} else if saldo >= numero {
-			saldo = saldo - numero
-			fmt.Println("saque realizado com sucesso")
-			fmt.Println("seu saldo atual é ", saldo)
-			} 
-		
-	 if escolha == 2 {
-		fmt.Println("escolha o valor que você quer depositar: ")
-		fmt.Scan(&numero)
-	 saldo = saldo + numero
-	 fmt.Println("seu saldo atual é ", saldo)
-	}
-
+ func sayGreeting(nome string) {
+     fmt.Println("OLÁ",nome)
 }
+func addNumber( numero1 int , numero2 int) int {
+
+return numero1  + numero2
+}
+func main() {
+	sayGreeting("Roberto")
+	resultado:= addNumber(10,20 )
+	fmt.Println( resultado )
+ }
