c0cb7ab02caa08a2f60d1c3160b565514f397e09
Aluno Dev 25 <ana.j.goncalves12@aluno.senai.sp>
2025-04-11 14:14:48 -0300

    depositar ou sacar

--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
  
- import (
-	"fmt") 
- 
- func sayGreeting(nome string) {
-     fmt.Println("OLÁ",nome)
-}
-func addNumber( numero1 int , numero2 int) int {
+import (
+   "fmt")
 
-return numero1  + numero2
+
+   var valorsaldo int
+   var opcao string
+   var valordeposito int
+   var valorsaque int
+
+func Saldo() {
+	   fmt.Println("digite seu saldo")
+	   fmt.Scan(&valorsaldo)
 }
-func main() {
-	sayGreeting("Roberto")
-	resultado:= addNumber(10,20 )
-	fmt.Println( resultado )
- }
+func Opcao(){
+   fmt.Println("voce quer 'sacar' ou 'depositar'?")
+   fmt.Scan(&opcao)
+}
+func Saque() {
+   
+	   if opcao == "sacar"{
+	   fmt.Println("quanto você deseja sacar?")
+	   fmt.Scan(&valorsaque)
+	   valorsaldo = valorsaldo - valorsaque
+	   fmt.Println("seu saldo é: ", valorsaldo)
+	   
+  }
+}
+func Deposito() {
+   if opcao == "depositar"{
+	   fmt.Println("quanto vc quer depositar?")
+	   fmt.Scan(&valordeposito)
+	   valorsaldo = valorsaldo + valordeposito
+
+	   fmt.Println("seu saldo é:", valorsaldo)
+   
+	   }
+}
+	   
+func main (){
+Saldo()
+Opcao()
+Saque()
+Deposito()
+}
