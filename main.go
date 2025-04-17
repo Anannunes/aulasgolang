5339f5cc1ec9ad2d8ce851d6bc4cfbd2bb49ec69
Aluno Dev 25 <ana.j.goncalves12@aluno.senai.sp>
2025-04-17 08:37:16 -0300

    divisao,soma e nul

--- main.go
+++ main.go
@@ -1,46 +1,35 @@
-package
+package main
  
 import (
-   "fmt")
+   "fmt"
+)
 
-
-   var valorsaldo int
-   var opcao string
-   var valordeposito int
-   var valorsaque int
-
-func Saldo() {
-	   fmt.Println("digite seu saldo")
-	   fmt.Scan(&valorsaldo)
+func dividir(dividendo int, divisor int)(int, string){
+	if divisor == 0 {
+		return 0, "Erro na divisão pro zero"
+	} 
+    return dividendo / divisor,"Sem erro"
 }
-func Opcao(){
-   fmt.Println("voce quer 'sacar' ou 'depositar'?")
-   fmt.Scan(&opcao)
+func operaçaobasica(a int, b int) (int, int, int){
+	soma := a + b
+	multiplicaçao := a * b
+	subtraçao := a - b
+	return soma,multiplicaçao,subtraçao
 }
-func Saque() {
+func main(){
+	resultado, erro:= dividir(10,2)
+
+	if erro != "sem erro" {
+		fmt.Println(erro)
+	}else {
+		fmt.Println("o resultado da divisão é:", resultado)
+	}
+	soma,mult,sub := operaçaobasica(10,2)
+		fmt.Println(soma)
+		fmt.Println(mult)
+		fmt.Println(sub)
+	
+}
+
+
    
-	   if opcao == "sacar"{
-	   fmt.Println("quanto você deseja sacar?")
-	   fmt.Scan(&valorsaque)
-	   valorsaldo = valorsaldo - valorsaque
-	   fmt.Println("seu saldo é: ", valorsaldo)
-	   
-  }
-}
-func Deposito() {
-   if opcao == "depositar"{
-	   fmt.Println("quanto vc quer depositar?")
-	   fmt.Scan(&valordeposito)
-	   valorsaldo = valorsaldo + valordeposito
-
-	   fmt.Println("seu saldo é:", valorsaldo)
-   
-	   }
-}
-	   
-func main (){
-Saldo()
-Opcao()
-Saque()
-Deposito()
-}
