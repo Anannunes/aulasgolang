ddb1ca8512875c811eaa716c67e09c1ec9a774e8
Aluno Dev 25 <ana.j.goncalves12@aluno.senai.sp>
2025-04-24 14:34:45 -0300

    bruce waayne

--- main.go
+++ main.go
@@ -3,21 +3,22 @@ package main
 import (
    "fmt")
 
-
 func main(){
-   alunoidade := make(map[string]int)
-   alunoidade ["fabiano"] = 16
-   alunoidade ["gisele"] = 18
-   alunoidade ["abobora"] = 3
-   alunoidade ["xerequinha"] = 15
-   fmt.Println("idade da xana", alunoidade["xerequinha"])
-   notasalunos := map[string]float64{
-	   "fabiano" : 7.9,
-	   "abobora" : 10,
-	   "xerequinha" : 3.8,
-	   "gisele" : 10,
-   } 
-   for nome, nota := range notasalunos{
-fmt.Printf("%s tirou a nota %.1f\n", nome, nota )
-   }
+   capitais ;=map[string]string{ 
+   "SP" : "São Paulo"
+   "RJ" : "Rio de Janeiro "
+   "ES" : "Espirito Santo"
 }
+ capitais["BH"] = "Belo Honizonte"
+
+ for k,v := range capitais{
+   fmt.Println("Sigla, Nome", k, v)
+ }
+ delete(capitais, "AC")
+
+ fork, v :=range capitais{
+   fmt.Println?("Sigla, Nome", k, v)
+   {
+
+   }
+ }
